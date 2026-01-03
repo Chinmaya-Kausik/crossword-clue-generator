c9135d1f4040fbcdd3bbc8c0c39d2b032d4a00d6
Chinmaya Kausik <39112310+Chinmaya-Kausik@users.noreply.github.com>
2026-01-03 01:02:01 -0700

    new training script

--- run_training.sh
+++ run_training.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-export PYENV_VERSION=py313
-cd /Users/ckausik/Documents/GitHub/crossword-clue-generator
-caffeinate -i python train_lora.py --max_length 128 2>&1 | tee training.log
+
+# Detect device
+if command -v nvidia-smi &> /dev/null; then
+    DEVICE="cuda"
+    BATCH_SIZE=16
+    GRAD_ACCUM=2
+else
+    DEVICE="mps"
+    BATCH_SIZE=4
+    GRAD_ACCUM=8
+fi
+
+python train_lora.py \
+    --device $DEVICE \
+    --batch_size $BATCH_SIZE \
+    --grad_accum $GRAD_ACCUM \
+    --max_length 128 \
+    2>&1 | tee training.log
